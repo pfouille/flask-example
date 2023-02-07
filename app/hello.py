ec73353ca9b82d501bf82e4ad7e9e79d6baa7637
pfouille <px.fouille@gmail.com>
2023-02-07 15:01:41 +0100

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def headers():
-    return '<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()])
+    return 'Hello Open lab BZH !!!'
 
 @app.route("/favicon.ico")
 def favicon():
