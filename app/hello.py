efbfa521f308ccef89a11d7c1bf572f6ead5d786
pfouille <px.fouille@gmail.com>
2023-06-19 08:32:01 +0200

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def headers():
-    return 'Hello Naval Group !!!'
+    return '<h1> Hello Naval Group !!! </h1>'
 
 @app.route("/favicon.ico")
 def favicon():
